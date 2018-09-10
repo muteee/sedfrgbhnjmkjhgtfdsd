4dfdc54ba454cfe711fa8d0bfb1e456f01116964
muteee <43130234+muteee@users.noreply.github.com>
2018-09-10 10:39:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = ".n";
+const prefix = "!";
 /////////////////////////
 ////////////////////////
 
@@ -343,3 +343,4 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
